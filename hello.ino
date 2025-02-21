8c4f8c215f707035940858145bb01d80fa5f71c8
Richard Riascos99 <riascosyelarichard@gmail.com>
2025-02-21 10:29:26 -0500

    create Hello.ino arduino Sketch

---
+++ hello.ino
@@ -0,0 +1,15 @@
+//sketch: hello.ino
+int const = Led =13;
+void setup() {
+  pinMode(LED OUPUT);
+  Serial.begin (9600);//baudios
+  digitalWrite(Led,Low)
+  Serial.print("Hello richi")
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
